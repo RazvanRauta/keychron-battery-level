8dba04d0f54af40ea3d7d610cbfff8479b803d1a
Razvan <40596596+RazvanRauta@users.noreply.github.com>
2025-12-19 10:28:48 +0200

    Initial Commit

---
+++ KeychronBattery/KeychronBatteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeychronBatteryApp.swift
+//  KeychronBattery
+//
+//  Created by Razvan on 19.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct KeychronBatteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
